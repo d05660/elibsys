5d5371f8eb32b3574a9ab6dc8393721d254f04c8
d05660 <d05660@163.com>
2018-04-17 19:35:20 +0800

    add some files.
    Signed-off-by: d05660 <d05660@163.com>

---
+++ ejb/src/main/java/org/cloud/demo1/action/Register.java
@@ -0,0 +1,8 @@
+package org.cloud.demo1.action;
+
+import javax.ejb.Local;
+
+@Local
+public interface Register {
+    public String register();
+}
---
+++ ejb/src/main/java/org/cloud/demo1/model/User.java
@@ -0,0 +1,67 @@
+package org.cloud.demo1.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.jboss.seam.annotations.Name;
+
+@Entity
+@Name("user")
+@Table(name = "user")
+public class User implements Serializable {
+
+    private static final long serialVersionUID = 1881413500711441951L;
+    private String username;
+
+    private String password;
+
+    private int id;
+
+    public User(int id, String password, String username) {
+        this.id = id;
+        this.password = password;
+        this.username = username;
+    }
+
+    public User() {
+    }
+
+    @NotNull
+    @Size(min = 5, max = 15)
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @NotNull
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @NotNull
+    @Size(min = 5, max = 15)
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+}
